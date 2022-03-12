14e9c6583cb30f24c36957486c8897ac7477c426
Kian <kianebtekari0@gmail.com>
2022-03-12 14:47:17 +0330

    update card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,68 +1,77 @@
 import React, { Component } from 'react';
+import sourosh from "./image/sourosh.png";
 
 class Cards extends Component {
     state = {  } 
     render() { 
         return (
             <body>
-                <div className="container-3 reveal"  id="container-3">
-                    <div className="card1" id="card">
+                <div className="wrapper">
+                    <div className="card1">
                         <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
                         </div>
                     </div>
-                    <div className="card2" id="card">
+                    <div className="card2">
                         <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
                         </div>
                     </div>
-                </div>
-                <div className="container-3"  id="container-3">
-                    <div className="card1" id="card">
+                    <div className="card3">
                         <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
-                        </div>
-                    </div>
-                    <div className="card2" id="card">
-                        <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
                         </div>
                     </div>
                 </div>
-
-                <div className="container-3"  id="container-3">
-                    <div className="card1" id="card">
+                <div className="wrapper">
+                    <div className="card4">
                         <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
                         </div>
                     </div>
-                    <div className="card2" id="card">
+                    <div className="card5">
                         <div className="content">
-                            <h3>Shoes</h3>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                            <a href="#">ارسال پیام</a>
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
+                        </div>
+                    </div>
+                    <div className="card6">
+                        <div className="content">
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="wrapper">
+                    <div className="card7">
+                        <div className="content">
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
+                        </div>
+                    </div>
+                    <div className="card8">
+                        <div className="content">
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
+                        </div>
+                    </div>
+                    <div className="card9">
+                        <div className="content">
+                            <h2>hello</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores dignissimos repellendus, esse consectetur, facilis accusamus soluta autem excepturi totam doloribus libero adipisci! Ipsum fuga praesentium id, eligendi eveniet blanditiis repudiandae.</p>
+                            <a href="" className='button'>Read more</a>
                         </div>
                     </div>
                 </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,35 +29,7 @@ class Home extends Component {
                 </section>
 
                     <h1 className='trends'>برترین محصولات</h1>
-                    <div class="container-3"  id="container-3">
-                        <div class="card1" id="card">
-                            <div class="content">
-                                <h3>Shoes</h3>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                    Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                    mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                                <a href="#">ارسال پیام</a>
-                            </div>
-                        </div>
-                        <div class="card2" id="card">
-                            <div class="content">
-                                <h3>Shoes</h3>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                    Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                    mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                                <a href="#">ارسال پیام</a>
-                            </div>
-                        </div>
-                        <div class="card3" id="card">
-                            <div class="content">
-                                <h3>Shoes</h3>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.   
-                                    Perferendis possimus quia deserunt, nisi, fuga sequi odit alias quam earum sed blanditiis quo,
-                                    mollitia quisquam ex.Ratione sed suscipit ad earum!</p>
-                                <a href="#">ارسال پیام</a>
-                            </div>
-                        </div>
-                    </div>
+
             </body>
         );
     }
